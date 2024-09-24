0f7eb9e7c833540503eedc956ee50fac9f282dcd
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2024-09-24 19:28:48 +0100

    Updates Storybook to latest (8.3)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,14 @@
-import type { StorybookConfig } from "@storybook/angular";
+import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
   },
 };
 export default config;
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,4 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/angular';
+
+import { fn } from '@storybook/test';
+
 import Button from './button.component';
 
 // More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
@@ -6,17 +9,13 @@ const meta: Meta<Button> = {
   title: 'Example/Button',
   component: Button,
   tags: ['autodocs'],
-  render: (args: Button) => ({
-    props: {
-      backgroundColor: null,
-      ...args,
-    },
-  }),
   argTypes: {
     backgroundColor: {
       control: 'color',
     },
   },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
 };
 
 export default meta;
