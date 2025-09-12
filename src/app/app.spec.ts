758fd2605f91eca0708b14633c3e49f68b0db2e7
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-09-12 17:46:21 +0100

    Configuration updates

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, testing-latest-angular-with-storybook-9');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, taskbox');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('testing-latest-angular-with-storybook-9');
+  protected readonly title = signal('taskbox');
 }
